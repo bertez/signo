e0674dc4814edafe251d00c86a8f3c51d6472e03
Berto Yáñez <berto@ber.to>
2019-11-26 19:45:25 +0100

    Implement Budget form

--- src/components/budget.jsx
+++ src/components/budget.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 
 export default function Budget({ service, product, description, closeBudget }) {
+  const API_ENDPOINT = 'https://api.signorotulacion.com/mail/budget';
+
   const [inputs, setInputs] = useState({
     name: '',
     phone: '',
     email: '',
-    comments: ''
+    comment: ''
   });
+
   const [finished, setFinished] = useState(false);
   const [error, setError] = useState('');
 
@@ -14,25 +17,52 @@ export default function Budget({ service, product, description, closeBudget }) {
     setInputs({ ...inputs, [e.target.name]: e.target.value });
   };
 
-  const handleFormSubmit = e => {
+  const handleFormSubmit = async e => {
     e.preventDefault();
-    // TODO: actually do this
-    console.log(inputs);
-    setFinished(true);
-    setTimeout(closeBudget, 2000);
+    setError('');
+
+    const payload = { service, product, description, ...inputs };
+
+    try {
+      const request = await fetch(API_ENDPOINT, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+      });
+
+      if (request.ok) {
+        setFinished(true);
+        setTimeout(closeBudget, 2000);
+      } else {
+        const response = await request.json();
+        throw new Error(response.message);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
   };
 
   return (
-    <section className="ly-modal">
+    <section
+      className="ly-modal"
+      onClick={e => {
+        if (e.target.matches('.ly-modal')) {
+          closeBudget();
+        }
+      }}
+    >
       <section className="modal-content">
         {finished ? (
-          <p class="message">
+          <p className="message">
             Gracias, en breve nos pondremos en contacto contigo.
           </p>
         ) : (
           <form onSubmit={handleFormSubmit} className="ly-form">
             <header>
-              <h2>Pedir presupuesto</h2>
+              <h2>Presupuesto</h2>
               <h3>{product}</h3>
             </header>
             <section className="fields">
@@ -42,39 +72,44 @@ export default function Budget({ service, product, description, closeBudget }) {
                   type="text"
                   name="name"
                   id="name"
+                  minLength="3"
+                  maxLength="100"
                   required
                   value={inputs.name}
                   onChange={handleInputChange}
                 />
               </fieldset>
-              <fieldset>
+              <fieldset className="line">
                 <label htmlFor="phone">Teléfono:</label>
                 <input
                   type="phone"
                   name="phone"
                   id="phone"
+                  minLength="9"
+                  maxLength="100"
                   required
                   value={inputs.phone}
                   onChange={handleInputChange}
                 />
               </fieldset>
-              <fieldset>
+              <fieldset className="line end">
                 <label htmlFor="email">Email:</label>
                 <input
                   type="email"
                   name="email"
                   id="email"
+                  required
                   value={inputs.email}
                   onChange={handleInputChange}
                 />
-                <span>opcional</span>
               </fieldset>
               <fieldset>
-                <label htmlFor="comments">Comentarios:</label>
+                <label htmlFor="comment">Comentarios: (opcional)</label>
                 <textarea
-                  name="comments"
-                  id="comments"
-                  value={inputs.comments}
+                  name="comment"
+                  id="comment"
+                  maxLength="1000"
+                  value={inputs.comment}
                   onChange={handleInputChange}
                 ></textarea>
               </fieldset>
@@ -82,6 +117,16 @@ export default function Budget({ service, product, description, closeBudget }) {
               <button type="submit">Enviar</button>
               <button onClick={closeBudget}>Cancelar</button>
             </section>
+            <footer>
+              {error && <p className="error">{error}</p>}
+
+              <p>
+                Al pulsar enviar aceptas las{' '}
+                <a href="/nota-legal" target="_blank" rel="noopener noreferrer">
+                  condiciones de servicio de Signo.
+                </a>
+              </p>
+            </footer>
           </form>
         )}
       </section>
